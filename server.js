a9558d62728ec165484fa55e9a282f77aae485a7
WhiteF326 <lume1a326.leno@gmail.com>
2021-02-26 00:59:40 +0900

    tekitou

--- server.js
+++ server.js
@@ -20,30 +20,53 @@
 import {Server} from "https://js.sabae.cc/Server.js"
 
 const users = [];
-const ranking = [];
+const ranking = [];                     // ランキングのデータを保持する変数
+const fetchNumber = 3;
 class body extends Server{
+
     api(path, prm){
-        //
-        let retobj = null;
+        let retObj = null;
         switch(path.split("/")[2]){
             // テンプレ
             case "":
                 break;
+
+            // 起動時ランキング
+            case "startup":
+                retObj = getRanking(prm.schoolName, fetchNumber);
+                break;
             
             // タイマーストップ
             case "timerstop":
-                /* 
-                    prm format
-                    {time: d(int)};
-                */
-                retobj = [Number(prm.time), Number(prm.distance)];
+                const point = (/* なんか適当な処理でポイント計算する */ 0);
+                ranking.push({schoolName: prm.schoolName, point: point, userName: prm.userName});
+                // retObj = [Number(prm.time), Number(prm.distance), String(prm.schoolName), String(prm.userName)];
+                retObj = getRanking(prm.schoolName, fetchNumber);
+                break;
+
+            // ユーザー登録
+            case "adduser":
                 break;
             
             // returns error
             default:
                 break;
         }
-        return retobj;
+        return retObj;
     }
+
+    //schoolNameに学校名を指定、fetchCountに必要なデータ件数を指定
+    getRanking(schoolName, fetchCount){
+        // schoolNameをキーにrankingを検索し、pointの降順にfetchCount件取得してreturnする
+        let retObj = [];
+        const tmpObj = ranking.filter(r => r.schoolName = schoolName).sort( (a, b) => {
+            return a.point > b.point ? -1 : 1;
+        });
+        for(let i = 0; i < fetchCount; i++){
+            if(tmpObj.length > i){retObj.push(tmpObj[i]);}else{retObj.push({schoolName: "", point: 0, userName: ""});}
+        }
+        return retObj;
+    }
+
 }
 new body(8001);
